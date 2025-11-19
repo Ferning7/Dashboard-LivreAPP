ef85c659c3886e510d55e7b3a1ee24415d3e71ce
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-11-19 11:49:30 -0300

    modo noturno 1.0

--- theme.js
+++ theme.js
@@ -1,11 +1,14 @@
 const btn = document.getElementById("toggle-theme");
+const icon = document.getElementById("theme-icon");
+
 
 btn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        btn.textContent = "☀️";
-    } else {
-        btn.textContent = "🌙";
-    }
+   if (document.body.classList.contains("dark-mode")) {
+    btn.innerHTML = `<img src="icons/sol.png" style="width: 40px">`;
+} else {
+    btn.innerHTML = `<img src="icons/lua.png" style="width: 40px">`;
+}
+
 });
